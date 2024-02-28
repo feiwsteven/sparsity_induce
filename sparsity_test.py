f4d7aaa25688ff3d8ae20e6ec9bbccf98fdaefd6
wafeiwang <wafeiwang@tencent.com>
2024-02-28 17:16:59 +0800

    C_relu activiation function

---
+++ sparsity_test.py
@@ -0,0 +1,38 @@
+import torch
+import torch.nn as nn
+import matplotlib.pyplot as plt 
+ 
+from torch import Tensor
+
+
+
+class ClipRelu(nn.Module):
+    def __init__(self, tau: float, m: float) -> None:
+        super(ClipRelu, self).__init__()
+        self.tau = tau
+        self.m = m
+
+    def forward(self, x: Tensor) -> Tensor:
+        if x < self.tau:
+            crelu = 0
+        elif x >= self.tau and x < self.tau + self.m:
+            crelu = x - self.tau
+        else:
+            crelu = self.m
+        return crelu
+
+if __name__ == '__main__':
+    
+ 
+    # create custom dataset 
+    x = torch.linspace(-5, 5, 100) 
+    k = ClipRelu(1, 2) 
+    y = k(x) 
+    
+    # plot the softplus function graph 
+    plt.plot(x, y) 
+    plt.grid(True) 
+    plt.title('Softplus Function') 
+    plt.xlabel('x') 
+    plt.ylabel('y') 
+    plt.show()
