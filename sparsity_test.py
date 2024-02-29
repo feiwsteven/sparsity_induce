ab7fe5f1643c4dda1dbb956ae2f1523058ce26b9
wafeiwang <wafeiwang@tencent.com>
2024-02-29 12:10:43 +0800

    update deep_net

--- sparsity_test.py
+++ sparsity_test.py
@@ -6,9 +6,9 @@ from torch import Tensor
 from torch.nn.modules import Linear
 
 
-class ClipRelu(nn.Module):
+class ClipReLu(nn.Module):
     def __init__(self, tau: float, m: float) -> None:
-        super(ClipRelu, self).__init__()
+        super(ClipReLu, self).__init__()
         self.tau = tau
         self.m = m
 
@@ -26,15 +26,25 @@ class ClipRelu(nn.Module):
 class DeepNet(nn.Module):
     def __init__(self, input_size: int, output_size: int, tau: float, m: float) -> None:
         super(DeepNet, self).__init__()
-        self.layers = nn.Sequential(nn.Linear(input_size), 300)
-        ClipRelu(tau, m)
+        CReLU = ClipReLu(tau, m)
+        self.layers = nn.Sequential(nn.Linear(input_size, 50), CReLU)
+        # Add 48 hidden layers
+        for _ in range(48):
+            self.layers.add_module("linear", nn.Linear(50, 50))
+            self.layers.add_module("cliprelu", CReLU)
+
+        # Last layer (hidden to output)
+        self.layers.add_module("output", nn.Linear(50, output_size))
+
+    def forward(self, x: Tensor) -> Tensor:
+        return self.layers(x)
 
 
 if __name__ == "__main__":
 
     # create custom dataset
     x = torch.linspace(-5, 5, 100)
-    k = ClipRelu(1, 2)
+    k = ClipReLu(1, 2)
     y = k(x)
 
     # plot the softplus function graph
@@ -44,3 +54,15 @@ if __name__ == "__main__":
     plt.xlabel("x")
     plt.ylabel("y")
     plt.show()
+
+    model = DeepNet(100, 100, 0.6, 1.2)
+
+    c_y = model(x)
+    # plot the softplus function graph
+    plt.plot(x, c_y.detach().numpy())
+    plt.grid(True)
+    plt.title("Softplus Function")
+    plt.xlabel("x")
+    plt.ylabel("y")
+    plt.show()
+
